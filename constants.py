11be712b19e1adc1fb59ee5e70124a3e1771f402
Yaroslav <93872617+YarikHrabovets@users.noreply.github.com>
2023-02-23 13:11:07 +0000

    Update constants.py

--- constants.py
+++ constants.py
@@ -5,7 +5,8 @@ USER = ' Your USERNAME'
 PASSWORD = 'Your PASSWORD'
 DB_NAME = 'Your DB NAME'
 
-GROUP_ID = -693400107
+#Any Group ID
+GROUP_ID = 0
 
 rent_names = ('1 раз на місяць', '2 рази на місяць', '1 раз в тиждень', '2 рази на тиждень',
               '3 рази на тиждень', '4 рази на тиждень', '5 разів на тиждень', '6 разів на тиждень',
